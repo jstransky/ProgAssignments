9139cdd52303a78715a8a1ca8d1b12f5cd2a3c95
jstransky <jackstransky@gmail.com>
2016-09-26 13:35:43 -0600

    new file:   ProgrammingAssignment1/.vs/ProgrammingAssignment1/v14/.suo 	new file:   ProgrammingAssignment1/ProgrammingAssignment1.sln 	new file:   ProgrammingAssignment1/ProgrammingAssignment1/App.config 	new file:   ProgrammingAssignment1/ProgrammingAssignment1/Program.cs 	new file:   ProgrammingAssignment1/ProgrammingAssignment1/ProgrammingAssignment1.csproj 	new file:   ProgrammingAssignment1/ProgrammingAssignment1/Properties/AssemblyInfo.cs 	new file:   ProgrammingAssignment1/ProgrammingAssignment1/bin/Debug/ProgrammingAssignment1.exe 	new file:   ProgrammingAssignment1/ProgrammingAssignment1/bin/Debug/ProgrammingAssignment1.exe.config 	new file:   ProgrammingAssignment1/ProgrammingAssignment1/bin/Debug/ProgrammingAssignment1.pdb 	new file:   ProgrammingAssignment1/ProgrammingAssignment1/bin/Debug/ProgrammingAssignment1.vshost.exe 	new file:   ProgrammingAssignment1/ProgrammingAssignment1/bin/Debug/ProgrammingAssignment1.vshost.exe.config 	new file:   ProgrammingAssignment1/ProgrammingAssignment1/bin/Debug/ProgrammingAssignment1.vshost.exe.manifest 	new file:   ProgrammingAssignment1/ProgrammingAssignment1/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   ProgrammingAssignment1/ProgrammingAssignment1/obj/Debug/ProgrammingAssignment1.csproj.FileListAbsolute.txt 	new file:   ProgrammingAssignment1/ProgrammingAssignment1/obj/Debug/ProgrammingAssignment1.csprojResolveAssemblyReference.cache 	new file:   ProgrammingAssignment1/ProgrammingAssignment1/obj/Debug/ProgrammingAssignment1.exe 	new file:   ProgrammingAssignment1/ProgrammingAssignment1/obj/Debug/ProgrammingAssignment1.pdb 	new file:   ProgrammingAssignment1/ProgrammingAssignment1/obj/Debug/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs 	new file:   ProgrammingAssignment1/ProgrammingAssignment1/obj/Debug/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs 	new file:   ProgrammingAssignment1/ProgrammingAssignment1/obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs

---
+++ ProgrammingAssignment1/ProgrammingAssignment1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ProgrammingAssignment1
+{
+    class Program
+    {
+        /// <summary>
+        /// Calculate the distance between two points and the angle between them
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            //greeting user
+            Console.WriteLine("Welcome.");
+            Console.WriteLine("This program will calculate the distance between two points");
+            Console.WriteLine("and the angle between those points.");
+
+            //prompt for user input
+            Console.Write("First point x value?: ");
+            float point1X = float.Parse(Console.ReadLine());
+            Console.Write("First point y value?: ");
+            float point1Y = float.Parse(Console.ReadLine());
+            Console.Write("Second point x value?: ");
+            float point2X = float.Parse(Console.ReadLine());
+            Console.Write("Second point y value?: ");
+            float point2Y = float.Parse(Console.ReadLine());
+
+            //Calculate difference between points 1 and 2
+            double deltaX = point2X - point1X;
+            double deltaY = point2Y - point1Y;
+
+            //Pythagorean finding hypotenuse
+            double hypotenuse = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+
+            //Finding the desired angle
+            double angle = Math.Atan2(deltaY , deltaX);
+
+            //radians to degrees
+            angle = angle * 57.2958;
+
+            //display results formated to 3 decimal places
+            Console.WriteLine("The distance between the two points is " + (hypotenuse.ToString("F3"))+ " units");
+            Console.WriteLine("and the angle between the points is " + (angle.ToString("F3")) + " degrees");
+            Console.ReadLine();
+            
+        }
+    }
+}
